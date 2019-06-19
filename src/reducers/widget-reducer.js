f395bdda6b9b3e767b73fdedf15581906d9d7941
nnamdi16 <nwabuokeinnamdi@gmail.com>
2019-06-19 15:18:50 +0100

    feat(widget-reducer.js): Create reducer for widget, for deleting a widget, and fetching a widget

---
+++ src/reducers/widget-reducer.js
@@ -0,0 +1,19 @@
+import * as types from '../actions/action-types';
+import _ from 'lodash';
+
+const initialState = {
+	widgets: []
+};
+
+const widgetReducer = (state = initialState, action) => {
+	switch (action.types) {
+		case types.GET_WIDGETS_SUCCESS:
+			return { ...state, widgets: action.widgets };
+
+		case types.DELETE_WIDGET_SUCCESS:
+			let newWidget = _.filter(state.widgets, widget => widget.id !== action.widgetId);
+			return { ...state, newWidget };
+		default:
+			return state;
+	}
+};
