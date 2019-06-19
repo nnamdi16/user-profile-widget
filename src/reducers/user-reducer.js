ce35bfbd5be6838b3a898f2800178559aa39444f
nnamdi16 <nwabuokeinnamdi@gmail.com>
2019-06-19 15:15:32 +0100

    feat(user-reducer.js): Create reducer for users, for creating a user, deleting a user and fetching a user

---
+++ src/reducers/user-reducer.js
@@ -0,0 +1,29 @@
+import * as types from '../actions/action-types';
+import _ from 'lodash';
+
+const initialState = {
+	users: [],
+	userProfile: {
+		repos: []
+	}
+};
+
+const userReducer = (state = initialState, action) => {
+	switch (action.type) {
+		case types.GET_USERS_SUCCESS:
+			return { ...state, users: action.users };
+
+		case types.DELETE_USER_SUCCESS:
+			//Use lodash to create a new user array without the user we want to be deleted.
+			const newUsers = _.filter(state.users, user => user.id !== action.userId);
+			return { ...state, users: newUsers };
+
+		case types.USER_PROFILE_SUCCESS:
+			return { ...state, userProfile: action.userProfile };
+
+		default:
+			return state;
+	}
+};
+
+export default userReducer;
